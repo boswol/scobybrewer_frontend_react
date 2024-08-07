8356f1c5207e25512ba25b1952b425b9b60f3b4f
boswol <nickberkvens7@gmail.com>
2024-08-07 22:35:27 +0200

    first page

---
+++ src/TrackingPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function TrackingPage() {
+  return (
+    <div className="TrackingPage">
+      <header className="TrackingPage-header">
+        <h1>This is the Second Page</h1>
+        <p>Welcome to the second page of the React app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default TrackingPage;
